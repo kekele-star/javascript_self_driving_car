aaa8e3fe5171331289da82286d13560c46d86877
kekele-star <esinamdemanya@gmail.com>
2023-08-02 13:01:52 +0000

    sensor class

---
+++ sensor.js
@@ -0,0 +1,24 @@
+class Sensor{
+    constructor(car){
+        this.car=car;
+        this.rayCount=3;
+        this.rayLength=100;
+        this.raySpread=Math.PI/4;
+
+        this.rays=[];
+    }
+
+    update(){
+        this.rays=[];
+        for(let i=0; i<this.rayCount; +ii){
+            const rayAngle=lerp(
+                this.raySpread/2,
+                -this.raySpread/2,
+                i/(this.rayCount-1)
+            );
+
+            
+        }
+    }
+
+}
